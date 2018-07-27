df7b08cfd2f4447e0ddd0c5bab55d4d50ebd6155
n-kats <n-kats19890214@hotmail.co.jp>
2018-07-28 03:05:40 +0900

    dataset rough_estimateコマンドの作成

--- mlbase/cli/main.py
+++ mlbase/cli/main.py
@@ -1,16 +1,13 @@
 from mlbase.utils.cli import Command
 from mlbase.template.cifar.train import train_cifar_command
+from mlbase.dataset.rough_estimate import rough_estimate_command
 
 
 def run():
     cmd = Command("mlbase", "機械学習を支援するコマンドです。")
 
     cmd_dataset = Command("dataset", "データセットに関するコマンドです。") << cmd
-    cmd_dataset.option("--input", help="入力データ")
-
-    @cmd_dataset
-    def _(args):
-        print(args)
+    rough_estimate_command() << cmd_dataset
 
     cmd_research = Command("research", "研究段階のもののコマンドです。") << cmd
     train_cifar_command() << cmd_research
---
+++ mlbase/dataset/rough_estimate.py
@@ -0,0 +1,18 @@
+from mlbase.utils.cli import Command
+
+
+def rough_estimate_command() -> Command:
+    cmd = Command("rough_estimate", "概算する")
+    data_size_cmd = Command("datasize", "データサイズを概算") << cmd
+    data_size_cmd.option("--width", type=int, required=True)
+    data_size_cmd.option("--height", type=int, required=True)
+    data_size_cmd.option("--channel", type=int, required=True)
+    data_size_cmd.option("--count", type=int, required=True)
+
+    @data_size_cmd
+    def _(args):
+        print(
+            args.width * args.height * args.channel * args.count // 1000000000,
+            "[GB]")
+
+    return cmd
